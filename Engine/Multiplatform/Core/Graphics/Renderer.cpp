9b38af54d4a145a6243027127715b1769871008d
Julien Nicolas <juli3nnicolas@gmail.com>
2015-10-13 21:52:41 +0200

    Started the modernization of the renderer. Added a brand new interface (the old one has yet to be removed). Implemented the uniform variable passing. The draw method is now private. Renderer has become an abstract class. Effective rendering will occur in specialized classes. Still remains some debug code.
    Work still in progress.

--- Engine/Multiplatform/Core/Graphics/Renderer.cpp
+++ Engine/Multiplatform/Core/Graphics/Renderer.cpp
@@ -3,11 +3,11 @@
 #include <cstring>
 
 #ifdef NEP_WINDOWS
-	#define WIN32_LEAN_AND_MEAN
-	#include <Windows.h>
-	
 	#include <GL/glew.h>
 #endif
+
+using namespace Neptune;
+
 
 // U T I L I T Y   F U N C T I O N S
 
@@ -20,12 +20,30 @@ static void setAsIdentityMatrix(GLfloat m[4][4])
 	m[3][0] = 0; m[3][1] = 0; m[3][2] = 0; m[3][3] = 1;
 }
 
+// U P D A T A B L E   I N T E R F A C E
+
+bool Renderer::init()
+{
+
+}
+
+bool Renderer::update()
+{
+	draw();
+}
+
+void Renderer::terminate()
+{
+
+}
+
+
 // R E N D E R E R ' S   M E T H O D S
 
 
 Neptune::Renderer::Renderer():
 	m_renderPgm(0), m_vertexArrayObject(0),
-	m_vertexIndicesType(0)
+	m_vertexIndicesType(0), m_nbverticesToRender(0)
 {
 	setBackgroundColor( 0.0f, 0.0f, 0.0f, 1.0f );
 	setAsIdentityMatrix( m_mvMatrix   );
@@ -37,6 +55,215 @@ Neptune::Renderer::~Renderer()
 	deleteRenderingProgram();
 	//deleteVAO();
 }
+
+GraphicalProgram& Renderer::createProgram()
+{
+	size_t i = m_programs.size();
+	m_programs.emplace_back();
+
+	return m_programs[i];
+}
+
+static void SetSingleValuedUniform(s32 location, const GraphicalProgram::UniformVarInput& var)
+{
+	switch( var.getDataType() )
+	{
+	case GraphicalProgram::Types::FLOAT:
+		glUniform1f( location, *(float*) var.getData() );
+		break;
+
+	case GraphicalProgram::Types::U32:
+		glUniform1ui( location, *(u32*) var.getData() );
+		break;
+
+	case GraphicalProgram::Types::S32:
+		glUniform1i( location, *(s32*) var.getData() );
+		break;
+
+	default:
+		NEP_ASSERT( false );
+		break;
+	}
+}
+
+static void SetVectorialUniform(s32 location, const GraphicalProgram::UniformVarInput& var)
+{
+	switch( var.getDataType() )
+	{
+
+	////////////////////////////////////////////////
+	case GraphicalProgram::Types::FLOAT:
+		
+		switch ( var.getNbRows() )
+		{
+		case 2:
+			glUniform2fv( location, 1, (float*) var.getData() );
+			break;
+
+		case 3:
+			glUniform3fv( location, 1, (float*) var.getData() );
+			break;
+
+		case 4:
+			glUniform4fv( location, 1, (float*) var.getData() );
+			break;
+
+		default:
+			NEP_ASSERT( false );
+			break;
+		}
+
+		break;
+	/////////////////////////////////////////////////
+
+
+	///////////////////////////////////////////////////
+	case GraphicalProgram::Types::U32:
+
+		switch(var.getNbRows())
+		{
+		case 2:
+			glUniform2uiv( location, 1, (u32*) var.getData() );
+			break;			 	       		  				 
+							 	       		  				 
+		case 3:				 	       		  				 
+			glUniform3uiv( location, 1, (u32*) var.getData() );
+			break;			 	       		  				 
+							 	       		  				 
+		case 4:				 	       		  				 
+			glUniform4uiv( location, 1, (u32*) var.getData() );
+			break;
+
+		default:
+			NEP_ASSERT(false);
+			break;
+		}
+
+		break;
+	////////////////////////////////////////////////////
+
+		
+	////////////////////////////////////////////////////
+	case GraphicalProgram::Types::S32:
+
+		switch(var.getNbRows())
+		{
+		case 2:
+			glUniform2iv(location,1,(s32*)var.getData());
+			break;
+
+		case 3:
+			glUniform3iv(location,1,(s32*)var.getData());
+			break;
+
+		case 4:
+			glUniform4iv(location,1,(s32*)var.getData());
+			break;
+
+		default:
+			NEP_ASSERT(false);
+			break;
+		}
+
+		break;
+	///////////////////////////////////////////////////
+	
+	
+	default:
+		NEP_ASSERT( false );
+		break;
+	}
+}
+
+static void SetMatrixUniform(s32 location, const GraphicalProgram::UniformVarInput& var)
+{
+	NEP_ASSERT( var.getDataType() == GraphicalProgram::Types::FLOAT );
+
+	if ( var.getNbRows() == var.getNbColumns() ) // Square matrix
+	{
+		switch ( var.getNbColumns() )
+		{
+		case 2:
+			glUniformMatrix2fv( location, 1, false, (float*) var.getData() );
+			break;
+
+		case 3:
+			glUniformMatrix3fv( location, 1, false, (float*) var.getData() );
+			break;
+
+		case 4:
+			glUniformMatrix4fv( location, 1, false, (float*) var.getData() );
+			break;
+		}
+	}
+	else
+	{
+		switch ( var.getNbColumns() )
+		{
+		case 2:
+			var.getNbRows() == 3 ? 
+				glUniformMatrix2x3fv(location, 1, false, (float*) var.getData()) : 
+				glUniformMatrix2x4fv(location, 1, false, (float*) var.getData());
+			break;
+
+		case 3:
+			var.getNbRows() == 2 ? 
+				glUniformMatrix3x2fv(location, 1, false, (float*) var.getData()) : 
+				glUniformMatrix3x4fv(location, 1, false, (float*) var.getData());
+			break;
+
+		case 4:
+			var.getNbRows() == 2 ? 
+				glUniformMatrix4x2fv(location, 1, false, (float*) var.getData()) : 
+				glUniformMatrix4x3fv(location, 1, false, (float*) var.getData());
+			break;
+		}
+	}
+}
+
+static void SetUniform(s32 location, const GraphicalProgram::UniformVarInput& var)
+{
+	// glUniformMatrix4fv( location, 1, GL_FALSE, (const float*) var.getData() );
+
+	// First check if the data is a single value, a vector or a matrix
+
+	if ( var.getNbRows() > 1 ) // It's either a vector or a matrix
+	{
+		if ( var.getNbColumns() > 1 ) // It's a vector
+		{
+			SetVectorialUniform( location, var );
+		}
+		else // It's a matrix
+		{
+			SetMatrixUniform( location, var );
+		}
+	}
+	else // It's a single value
+	{
+		SetSingleValuedUniform( location, var );
+	}
+}
+
+void Renderer::bindUniformVars()
+{
+	// Browse the programs
+	std::vector<GraphicalProgram>::const_iterator it_end = m_programs.cend();
+	for ( std::vector<GraphicalProgram>::const_iterator it = m_programs.cbegin(); it != it_end; ++it )
+	{
+		// Browse the uniform vars
+		GraphicalProgram::ConstUniformVarIterator uni_it_end = it->uniformVarCEnd();
+		for ( GraphicalProgram::ConstUniformVarIterator uni_it = it->uniformVarCBegin(); uni_it != uni_it_end; ++uni_it )
+		{
+			s32 location = glGetUniformLocation( it->getId(), uni_it->getName() );
+			NEP_ASSERT( location >= 0 && glGetError() != GL_INVALID_VALUE && glGetError() != GL_INVALID_OPERATION);
+
+			SetUniform( location, *uni_it );
+			//glUniformMatrix4fv(location,1,GL_FALSE,&m_mvMatrix[0][0]);
+		}
+	}
+}
+
+// Old interface
 
 void Neptune::Renderer::setVAO(u32 vao_handle)
 {
@@ -90,13 +317,45 @@ void Neptune::Renderer::draw()
 	glClear( GL_DEPTH_BUFFER_BIT );
 
 	// Prepare vertex shader
-	glBindVertexArray(m_vertexArrayObject);
+	//glBindVertexArray(m_vertexArrayObject);
 	glUseProgram(m_renderPgm);
 	sendCTMMatrix();
 
 	// Draw-call
-	glDrawElements(GL_TRIANGLES, m_nbverticesToRender, m_vertexIndicesType, 0); // Watch out! the third value corresponds to the vertex-index type
-	//glDrawArrays(/*GL_PATCHES*/ GL_TRIANGLES, 0, 3*12); // GL_TRIANGLES without any tess shader
+	/*glDrawElements(GL_TRIANGLES, m_nbverticesToRender, m_vertexIndicesType, 0); // Watch out! the third value corresponds to the vertex-index type
+	switch ( glGetError() )
+	{
+	case GL_NO_ERROR:
+		NEP_LOG("All good");
+		break;
+
+	case GL_INVALID_ENUM:
+		NEP_LOG("Invalid enum");
+		break;
+
+	case GL_INVALID_VALUE:
+		NEP_LOG("invalid value");
+	break;
+
+	case GL_INVALID_OPERATION:
+		NEP_LOG("invalid op");
+		break;
+
+	case GL_INVALID_FRAMEBUFFER_OPERATION:
+		NEP_LOG("invalid fb op");
+		break;
+
+	case GL_OUT_OF_MEMORY:
+		NEP_LOG("Out of mem");
+	break;
+
+	default:
+		NEP_LOG("WTF");
+		break;
+	}*/
+	
+	
+	glDrawArrays(/*GL_PATCHES*/ GL_TRIANGLES, 0, m_nbverticesToRender ); // GL_TRIANGLES without any tess shader
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);	// Only draws the edges
 }
 
--- Engine/Multiplatform/Core/Graphics/Renderer.h
+++ Engine/Multiplatform/Core/Graphics/Renderer.h
@@ -1,24 +1,43 @@
-#ifndef MOST_BASIC_RENDERER_H_
-#define MOST_BASIC_RENDERER_H_
+#pragma once
 
+#include "StdInterface/Updatable.h"
 #include "System/Type/Integers.h"
+#include "Graphics/GraphicalProgram.h"
+
+#include <vector>
 
 namespace Neptune
 {
-	class Renderer
+	class Renderer : public Updatable
 	{
 	public:
+		enum DrawingPrimitive : u8
+		{
+			TRIANGLES
+		};
+
+		// New interface
+
 		Renderer();
 		virtual ~Renderer();
+		Renderer(const Renderer&)            = delete;
+		Renderer& operator=(const Renderer&) = delete;
 
-		// Display graphics to the window
-		void draw();
+		bool init()      override;
+		bool update()    override;
+		void terminate() override;
+
+		GraphicalProgram& createProgram();
+
+		void setNbverticesToRender(u32 nbvertices)     { m_nbverticesToRender = nbvertices; }
+		void setDrawingPrimitve(DrawingPrimitive dp)   { m_drawingPrimitive = dp;           }
+
+		// Old interface
 
 		void setRenderingPgm(u32 pgm_id)           { m_renderPgm = pgm_id;              }
 		void deleteRenderingProgram();					  
 		void setVAO(u32 vao_handle);					  
 		void deleteVAO();								  
-		void setNbverticesToRender(u32 nbvertices) { m_nbverticesToRender = nbvertices; }
 		void setVertexIndicesType(u16 type)        { m_vertexIndicesType = type;        }
 
 		void setBackgroundColor(float r, float g, float b, float a); // A value between 0.0f and 1.0f is expected for each parameter
@@ -27,21 +46,25 @@ namespace Neptune
 		void setProjMatrix(const float proj[4][4]);
 
 	protected:
+		virtual void draw() = 0; /// Display graphics to the window
+
+		// Last version
+		u32                           m_nbverticesToRender;  /// Number of vertices that will be passed to the vertex shader
+		DrawingPrimitive              m_drawingPrimitive;    /// Which OpenGL drawing primitive must be used for rendering (Triangles, TriangleFan...)
+		std::vector<u32>              m_glResourcesId;       /// Array of all the OpenGl resources' id allocated by the renderer. Its inner organization and what it contains (e.g. VAOs, VBOs...) are up to the specialized renderer.              
+		std::vector<GraphicalProgram> m_programs;            /// Contains all the OpenGL programs that must be applied by a renderer
+
+
+		// Previous version
 		u32    m_renderPgm;
 		u32    m_vertexArrayObject;
-		u32    m_nbverticesToRender;
 		u16    m_vertexIndicesType;
 		float  m_backgroundColor[4];
 		float  m_mvMatrix[4][4];
 		float  m_projMatrix[4][4];
 
 	private:
+		void bindUniformVars();
 		void sendCTMMatrix(); // CTM stands for Current Transform Matrix = Proj*MV
-
-		// Forbidden use
-		Renderer(const Renderer&){}
-		Renderer& operator=(const Renderer&) {return *this;}
 	};
 }
-
-#endif
