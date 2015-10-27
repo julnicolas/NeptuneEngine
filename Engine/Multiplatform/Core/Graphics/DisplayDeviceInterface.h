2c1bd43c05cccea6bde68661a42e975c25b4cab6
Julien Nicolas <juli3nnicolas@gmail.com>
2015-10-27 05:08:56 +0100

    Added a function to clear buffers before a draw call

--- Engine/Multiplatform/Core/Graphics/DisplayDeviceInterface.h
+++ Engine/Multiplatform/Core/Graphics/DisplayDeviceInterface.h
@@ -9,19 +9,22 @@ namespace Neptune
 		typedef void* WindowHandle;
 		typedef void* GraphicalContextHandle;
 
-		// Create a window to host the graphical context
+		/// Creates a window to host the graphical context
 		WindowHandle CreateWindow(const char* name, u32 width, u32 height, bool fullScreen = false);
 
-		// Destruct the window that holds the graphical context
+		/// Destructs the window that holds the graphical context
 		void DestroyWindow(WindowHandle handle);
 
-		// Creates a graphical context as well as the app's main window
+		/// Creates a graphical context as well as the app's main window
 		GraphicalContextHandle CreateGraphicalContext(WindowHandle window, u8 minCtxtVersion,u8 maxCtxtVersion);
 		
-		// Destruct a graphical context and the main window
+		/// Destructs a graphical context and the main window
 		void DestroyGraphicalContext(GraphicalContextHandle handle);
 
-		// Swap the front and the back buffer when double buffering is enabled
+		/// Clears any relevant buffer before the rendering stage
+		void ClearBuffers(float backGroundColor[4]);
+
+		/// Swaps the front and the back buffer when double buffering is enabled
 		void SwapBuffer(WindowHandle handle);
 
 		// Getters
