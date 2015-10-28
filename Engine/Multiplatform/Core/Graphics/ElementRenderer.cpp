9fc65185596b12dfbb865b401cdaa2deb373f504
Julien Nicolas <juli3nnicolas@gmail.com>
2015-10-28 03:28:14 +0100

    Implemented the element renderer (uses glDrawElements when making a draw-call).

---
+++ Engine/Multiplatform/Core/Graphics/ElementRenderer.cpp
@@ -0,0 +1,159 @@
+#include "Graphics/ElementRenderer.h"
+#include "Graphics/IncludeOpenGL.h"
+#include "Debug/NeptuneDebug.h"
+
+using namespace Neptune;
+
+ElementRenderer::ElementRenderer()
+{
+	// Generate the index buffer
+	glGenBuffers( 1, &m_indexBuffer );
+}
+
+ElementRenderer::~ElementRenderer()
+{
+	// Free the index buffer
+	glDeleteBuffers( 1, &m_indexBuffer ); // Class specific
+
+	// Free the vbos
+	GraphicalProgramIterator it_end = m_programs.end();
+	for(GraphicalProgramIterator it = m_programs.begin(); it != it_end; ++it) // Browse every program
+	{
+		// Look through the vertex attributes
+		std::vector<u32>& vbo = m_vbos[&(**it)];
+		size_t vbo_size = vbo.size();
+
+		for(size_t i = 0; i < vbo_size; i++)
+			glDeleteBuffers(1,&vbo[i]);
+	}
+}
+
+void ElementRenderer::setIndexBufferData(const void* data, u32 dataSize, ElementRenderer::IndexType dataType)
+{
+	m_indexType = dataType;
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_indexBuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, dataSize, data, GL_STATIC_DRAW);
+}
+
+bool ElementRenderer::init()
+{
+	NEP_ASSERT(Renderer::init());
+
+	// First reading through the programs to get the one that uses the biggest number of parameters
+	size_t biggest_nb_pms = 0;
+
+	ConstGraphicalProgramIterator it_end = m_programs.cend();
+	for(ConstGraphicalProgramIterator it = m_programs.cbegin(); it != it_end; ++it)
+		biggest_nb_pms = (biggest_nb_pms < (*it)->getNbVertexAttributes()) ? (*it)->getNbVertexAttributes() : biggest_nb_pms;
+
+	// Allocate the VBOs to store the vertex attributes and fill them
+	GLuint* vbos_handle = new GLuint[biggest_nb_pms];
+
+	// Iterate over the programs to set their parameter data
+	for(ConstGraphicalProgramIterator it = m_programs.cbegin(); it != it_end; ++it)
+	{
+		// Allocate the program's VBOs
+		const u8 nb_attribs = (*it)->getNbVertexAttributes();
+		glGenBuffers(nb_attribs,vbos_handle);
+
+		// Populate them and enable them to be used in the graphical pipeline
+		u8 attrib_index = 0;
+		GraphicalProgram::ConstShaderAttributeIterator att_end = (*it)->shaderAttributeCEnd();
+		for(GraphicalProgram::ConstShaderAttributeIterator att = (*it)->shaderAttributeCBegin(); att != att_end; ++att,attrib_index++)
+		{
+			// Fill the buffers
+			glBindBuffer(GL_ARRAY_BUFFER,vbos_handle[attrib_index]);
+			glBufferData(GL_ARRAY_BUFFER,att->m_size,att->m_data,GL_STATIC_DRAW);
+			m_vbos[&(**it)].push_back(vbos_handle[attrib_index]);
+
+			// Enable the shader's input interfaces
+			glEnableVertexAttribArray(att->m_layout);
+		}
+	}
+	delete[] vbos_handle;
+
+	return true;
+}
+
+void ElementRenderer::terminate()
+{
+	Renderer::terminate();
+}
+
+static GLenum MapDrawingPrimitive(Renderer::DrawingPrimitive const p)
+{
+	GLenum primitive = GL_INVALID_ENUM;
+
+	switch(p)
+	{
+	case Renderer::TRIANGLES:
+		primitive = GL_TRIANGLES;
+		break;
+	}
+
+	return primitive;
+}
+
+static GLenum MapType(const GraphicalProgram::Types type)
+{
+	GLenum gl_type = GL_INVALID_ENUM;
+
+	switch(type)
+	{
+	case GraphicalProgram::Types::U32:
+		gl_type = GL_UNSIGNED_INT;
+		break;
+
+	case GraphicalProgram::Types::S32:
+		gl_type = GL_INT;
+		break;
+
+	case GraphicalProgram::Types::FLOAT:
+		gl_type = GL_FLOAT;
+		break;
+	}
+
+	return gl_type;
+}
+
+static GLenum MapType(const ElementRenderer::IndexType type)
+{
+	GLenum gl_type = GL_INVALID_ENUM;
+
+	switch(type)
+	{
+	case ElementRenderer::IndexType::U8:
+		gl_type = GL_UNSIGNED_BYTE;
+		break;
+
+	case ElementRenderer::IndexType::U16:
+		gl_type = GL_UNSIGNED_SHORT;
+		break;
+
+	case ElementRenderer::IndexType::U32:
+		gl_type = GL_UNSIGNED_INT;
+		break;
+	}
+
+	return gl_type;
+}
+
+void ElementRenderer::draw()
+{
+	glDrawElements( MapDrawingPrimitive(m_drawingPrimitive), m_nbverticesToRender, MapType(m_indexType), 0 );
+}
+
+void ElementRenderer::bindShaderAttributes(const GraphicalProgram& pgm)
+{
+	// Bind the indices
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_indexBuffer);
+	
+	// Bind every pgm's vertex attribute to its vbo
+	u32 i = 0;
+	GraphicalProgram::ConstShaderAttributeIterator att_end = pgm.shaderAttributeCEnd();
+	for(GraphicalProgram::ConstShaderAttributeIterator att = pgm.shaderAttributeCBegin(); att != att_end; ++att,i++)
+	{
+		glBindBuffer(GL_ARRAY_BUFFER,m_vbos[&pgm][i]);
+		glVertexAttribPointer(att->m_layout,att->m_nbComponents,MapType(att->m_type),GL_FALSE,0,NULL);
+	}
+}
---
+++ Engine/Multiplatform/Core/Graphics/ElementRenderer.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "Graphics/Renderer.h"
+#include <unordered_map>
+
+namespace Neptune
+{
+	class ElementRenderer: public Neptune::Renderer
+	{
+	public:
+		enum IndexType : u8
+		{
+			U8,
+			U16,
+			U32
+		};
+
+		ElementRenderer();
+		virtual ~ElementRenderer();
+		ElementRenderer(const ElementRenderer&)            = delete;
+		ElementRenderer& operator=(const ElementRenderer&) = delete;
+
+		bool init()      override; /// Allocate all the VBOs to store the vertex attributes
+		void terminate() override;
+
+		void setIndexBufferData(const void* data, u32 dataSize, IndexType dataType);
+
+	protected:
+		void draw() override;                                                   /// Display graphics to the window
+		void bindShaderAttributes(const GraphicalProgram& pgm) override;
+
+	private:
+		u32                                                           m_indexBuffer;
+		IndexType                                                     m_indexType;
+		std::unordered_map<const GraphicalProgram*,std::vector<u32> > m_vbos; /// Contains all vbos used by a graphical program
+	};
+}
