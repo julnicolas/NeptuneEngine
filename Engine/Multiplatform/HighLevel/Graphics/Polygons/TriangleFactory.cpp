0420fb446afba9719b01cec2d141a52b5c67c7da
Julien Nicolas <juli3nnicolas@gmail.com>
2015-11-24 03:27:29 +0100

    Improved the triangle factory.

--- Engine/Multiplatform/HighLevel/Graphics/Polygons/TriangleFactory.cpp
+++ Engine/Multiplatform/HighLevel/Graphics/Polygons/TriangleFactory.cpp
@@ -7,53 +7,12 @@ using namespace Neptune;
 
 TriangleFactory::TriangleFactory()
 {
-	// Set vertex data
-	float t1[] =
-	{
-		0.0f,0.0f,0.0f,
-		0.5f,0.0f,0.0f,
-		0.0f,0.5f,0.0f
-	};
-	for(unsigned int i = 0; i < sizeof(t1)/sizeof(float); i++)
-		m_vertices.push_back(t1[i]);
+	initData(0.5f, 0.5f, 0.5f);
+}
 
-	// Set color data
-	float c1[] =
-	{
-		1.0f,0.0f,0.0f,
-		0.0f,1.0f,0.0f,
-		0.0f,0.0f,1.0f
-	};
-	for(unsigned int i = 0; i < sizeof(c1)/sizeof(float); i++)
-		m_colors.push_back(c1[i]);
-
-	// Create the shader attributes
-
-	GraphicalProgram::ShaderAttribute t1_data =
-	{
-		0,                                // layout
-		GraphicalProgram::Types::FLOAT,  // Type
-		3,                              // nb components per value
-		sizeof(t1),                    // data size
-		&m_vertices[0]                // data
-	};
-
-	GraphicalProgram::ShaderAttribute c1_data =
-	{
-		1,                                // layout
-		GraphicalProgram::Types::FLOAT,  // Type
-		3,                              // nb components per value
-		sizeof(c1),                    // data size
-		&m_colors[0]                  // data
-	};
-
-	// Add them to the factory
-	m_shaderAttributes.push_back(t1_data);
-	m_shaderAttributes.push_back(c1_data);
-
-	// Set shader names
-	m_vertexShaderName   = "Resources/Shaders/VertexBuffer.vert";
-	m_fragmentShaderName = "Resources/Shaders/VertexBuffer.frag";
+TriangleFactory::TriangleFactory(float r, float g, float b)
+{
+	initData(r, g, b);
 }
 
 VAOView* TriangleFactory::create()
@@ -81,4 +40,50 @@ VAOView* TriangleFactory::create()
 	}
 
 	return v;
+}
+
+void TriangleFactory::initData(float r, float g, float b)
+{
+	// Set vertex data
+	float t1[] =
+	{
+		0.0f,0.0f,0.0f,
+		0.5f,0.0f,0.0f,
+		0.0f,0.5f,0.0f
+	};
+	for(unsigned int i = 0; i < sizeof(t1)/sizeof(float); i++)
+		m_vertices.push_back(t1[i]);
+
+	// Set color data
+	m_colors.push_back(r); m_colors.push_back(g); m_colors.push_back(b);
+	m_colors.push_back(r); m_colors.push_back(g); m_colors.push_back(b);
+	m_colors.push_back(r); m_colors.push_back(g); m_colors.push_back(b);
+
+	// Create the shader attributes
+
+	GraphicalProgram::ShaderAttribute t1_data =
+	{
+		0,                                // layout
+		GraphicalProgram::Types::FLOAT,  // Type
+		3,                              // nb components per value
+		sizeof(t1),                    // data size
+		&m_vertices[0]                // data
+	};
+
+	GraphicalProgram::ShaderAttribute c1_data =
+	{
+		1,                                      // layout
+		GraphicalProgram::Types::FLOAT,        // Type
+		3,                                    // nb components per value
+		m_colors.size()*sizeof(m_colors[0]), // data size
+		&m_colors[0]                        // data
+	};
+
+	// Add them to the factory
+	m_shaderAttributes.push_back(t1_data);
+	m_shaderAttributes.push_back(c1_data);
+
+	// Set shader names
+	m_vertexShaderName   = "Resources/Shaders/VertexBuffer.vert";
+	m_fragmentShaderName = "Resources/Shaders/VertexBuffer.frag";
 }
--- Engine/Multiplatform/HighLevel/Graphics/Polygons/TriangleFactory.h
+++ Engine/Multiplatform/HighLevel/Graphics/Polygons/TriangleFactory.h
@@ -9,10 +9,14 @@ namespace Neptune
 	{
 	public:
 		TriangleFactory();
+		TriangleFactory(float r, float g, float b);
 		virtual ~TriangleFactory()                         = default;
 		TriangleFactory(const TriangleFactory&)            = default;
 		TriangleFactory& operator=(const TriangleFactory&) = default;
 
 		VAOView* create() override; /// Allocates a new VAOView on the heap. Class users must handle the object's deallocation.
+
+	private:
+		void initData(float r, float g, float b);
 	};
 }
