ef9c28526185257e0a3c27c019d569e9020be646
Julien Nicolas <juli3nnicolas@gmail.com>
2015-11-30 06:02:51 +0100

    Implemented the FNV1A hash function in both its 64 and 32 bit version

---
+++ Engine/Multiplatform/Core/System/Hashing/FastHashFunctions.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "System/Type/Integers.h"
+
+namespace Neptune
+{
+	inline u64 Fnv1a64(u8* data, u64 size)
+	{
+		const u64 FNV_OFFSET_BASIS = 14695981039346656037ull;
+		const u64 FNV_PRIME        = 1099511628211ull;
+		u64 hash                   = FNV_OFFSET_BASIS;
+
+		for(u64 i = 0; i < size; i++)
+		{
+			hash ^= data[i];
+			hash *= FNV_PRIME;
+		}
+
+		return hash;
+	}
+
+	inline u32 Fnv1a32(u8* data, u32 size)
+	{
+		const u32 FNV_OFFSET_BASIS = 0x811C9DC5;
+		const u32 FNV_PRIME        = 0x01000193;
+		u32 hash                   = FNV_OFFSET_BASIS;
+
+		for(u32 i = 0; i < size; i++)
+		{
+			hash ^= data[i];
+			hash *= FNV_PRIME;
+		}
+
+		return hash;
+	}
+}
