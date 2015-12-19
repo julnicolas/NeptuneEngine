c4dda815e52c9b1d891d9a247e30a179d2f53692
Julien Nicolas <juli3nnicolas@gmail.com>
2015-12-19 18:17:10 +0100

    Fixed a linking error

--- Engine/Win32/Core/Math/Vectors/Mat4Implementation.h
+++ Engine/Win32/Core/Math/Vectors/Mat4Implementation.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <glm/mat4x4.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 namespace Neptune
 {
@@ -8,11 +9,11 @@ namespace Neptune
 	class Mat4: public glm::tmat4x4<T>
 	{
 	public:
-		Mat4<T>()                          = default;
+		Mat4<T>(): glm::tmat4x4<T>(1)      {}                          
 		~Mat4<T>()                         = default;
 		Mat4<T>(const Mat4<T>&)            = default;
 		Mat4<T>& operator=(const Mat4<T>&) = default;
 
-		const T* getPtr() const { return glm::value_ptr(*this); }
+		const T* getPtr() const { return glm::value_ptr(* (tmat4x4<T>*) this); }
 	};
 }
