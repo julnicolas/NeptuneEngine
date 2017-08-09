76959a06c57d809ec8558589956fa4528b01f45a
Julien Nicolas <juli3nnicolas@gmail.com>
2017-08-10 00:20:03 +0100

    added a const qualifier

--- Engine/Multiplatform/Core/System/Hashing/FastHashFunctions.h
+++ Engine/Multiplatform/Core/System/Hashing/FastHashFunctions.h
@@ -4,7 +4,7 @@
 
 namespace Neptune
 {
-	inline u64 Fnv1a64(u8* data, u64 size)
+	inline u64 Fnv1a64(const u8* data, u64 size)
 	{
 		const u64 FNV_OFFSET_BASIS = 14695981039346656037ull;
 		const u64 FNV_PRIME        = 1099511628211ull;
@@ -19,7 +19,7 @@ namespace Neptune
 		return hash;
 	}
 
-	inline u32 Fnv1a32(u8* data, u32 size)
+	inline u32 Fnv1a32(const u8* data, u32 size)
 	{
 		const u32 FNV_OFFSET_BASIS = 0x811C9DC5;
 		const u32 FNV_PRIME        = 0x01000193;
