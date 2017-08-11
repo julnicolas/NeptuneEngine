7cfc30b30e9503b09cd07a9ef54a67fe08ea132e
Julien Nicolas <juli3nnicolas@gmail.com>
2017-08-11 22:23:55 +0100

    Added some logging for assertions in release mode

--- Engine/Multiplatform/Core/Debug/NeptuneDebug.h
+++ Engine/Multiplatform/Core/Debug/NeptuneDebug.h
@@ -3,18 +3,49 @@
 #include <cstdio>
 #include <cassert>
 
+
+/////////////////////////////////////////////////////////////////////////////
+//
+//								L O G S 
+//
+/////////////////////////////////////////////////////////////////////////////
+
 #ifndef NEP_FINAL
 	// Variadic macros have become standard since c++11
-	#define NEP_LOG(...) std::fprintf(stderr, __VA_ARGS__)
+	#define NEP_LOG(...) std::fprintf(stderr, __VA_ARGS__) // TODO: Change stderr by something more useful
 #else
 	// The statement between NEP_LOG's brackets won't be executed 
 	#define NEP_LOG(err) ((void) 0)
 #endif
 
+
+/////////////////////////////////////////////////////////////////////////////
+//
+//							A S S E R T I O N S
+//
+/////////////////////////////////////////////////////////////////////////////
+
 #define NEP_STATIC_ASSERT(expression, message) static_assert(expression, message)
 
 #ifdef NEP_DEBUG
 	#define NEP_ASSERT	assert
-#else
-	#define NEP_ASSERT
+#endif
+
+#ifdef NEP_RELEASE
+namespace Neptune
+{
+	namespace Debug
+	{
+		namespace Private
+		{
+			inline void LogAssert(bool _cond) { if (!_cond) NEP_LOG("Exception thrown: file: %s, line: %d", __FILE__, __LINE__); }
+		}
+	}
+}
+	#define NEP_ASSERT(_cond) Neptune::Debug::Private::LogAssert(_cond)
+#endif
+
+#ifdef NEP_FINAL
+	// The statement between the brackets won't be executed 
+	#define NEP_ASSERT(_cond) ((void) 0)
 #endif
