28e5d2ad3b7065f842258055867f2911a1f09139
Julien Nicolas <juli3nnicolas@gmail.com>
2017-08-20 22:57:14 +0100

    Added the PlanSpawner class

---
+++ Engine/Multiplatform/HighLevel/Graphics/Factories/PlanSpawner.cpp
@@ -0,0 +1,55 @@
+#include "Graphics/Factories/PlanSpawner.h"
+#include "Graphics/VAOView.h"
+#include "Debug/NeptuneDebug.h"
+
+using namespace Neptune;
+
+
+View* PlanSpawner::createViewAndSetUpRenderParameters()
+{		 
+	VAOView* v = new VAOView;
+
+	// Setup the triangle's renderer
+	v->setDrawingPrimitive(Renderer::DrawingPrimitive::TRIANGLE_STRIP);
+	v->setNbVerticesToRender(4);
+
+	return v;
+}		 
+		 
+bool PlanSpawner::createColorData(const Color& _color)
+{		 
+	m_colors.push_back(_color);
+	m_colors.push_back(_color);
+	m_colors.push_back(_color);
+	m_colors.push_back(_color);
+
+	return true;
+}		 
+		 
+bool PlanSpawner::createNormalData()
+{		 
+	m_normals.push_back(0.0f); m_normals.push_back(0.0f); m_normals.push_back(-1.0f); // First  vertex
+	m_normals.push_back(0.0f); m_normals.push_back(0.0f); m_normals.push_back(-1.0f); // Second vertex
+	m_normals.push_back(0.0f); m_normals.push_back(0.0f); m_normals.push_back(-1.0f); // Third  vertex
+	m_normals.push_back(0.0f); m_normals.push_back(0.0f); m_normals.push_back(-1.0f); // Fourth vertex
+
+	return true;
+}		 
+		 
+bool PlanSpawner::create2DTextureMapData()
+{
+	m_2DTexCoords.push_back(0.0f); m_2DTexCoords.push_back(1.0f); // First  vertex
+	m_2DTexCoords.push_back(1.0f); m_2DTexCoords.push_back(1.0f); // Second vertex
+	m_2DTexCoords.push_back(0.0f); m_2DTexCoords.push_back(0.0f); // Third  vertex
+	m_2DTexCoords.push_back(1.0f); m_2DTexCoords.push_back(0.0f); // Fourth vertex
+
+	return true;
+}
+
+void PlanSpawner::createVertexData()
+{
+	m_vertices.push_back(0.0f); m_vertices.push_back(0.0f); m_vertices.push_back(0.0f); // First  vertex
+	m_vertices.push_back(0.5f); m_vertices.push_back(0.0f); m_vertices.push_back(0.0f);	// Second vertex
+	m_vertices.push_back(0.0f); m_vertices.push_back(0.5f); m_vertices.push_back(0.0f);	// Third  vertex
+	m_vertices.push_back(0.5f); m_vertices.push_back(0.5f); m_vertices.push_back(0.0f);	// Fourth vertex
+}
---
+++ Engine/Multiplatform/HighLevel/Graphics/Factories/PlanSpawner.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "Graphics/Factories/ViewSpawner.h"
+
+namespace Neptune
+{
+	class  VAOView;
+	struct Color;
+
+	class PlanSpawner: public ViewSpawner
+	{
+	public:
+		//
+		// C O N S T R U C T O R S
+		//
+
+		PlanSpawner(GraphicsProgram* _pgm):
+			ViewSpawner(_pgm){}
+
+
+		//
+		// D E F A U L T   G E N E R A T E D   M E T H O D S
+		//
+
+		virtual ~PlanSpawner()										= default;
+		PlanSpawner(const PlanSpawner&)								= delete;
+		virtual PlanSpawner& operator=(const PlanSpawner&)			= delete;
+
+
+		//
+		// S P E C I A L I Z E D   M E T H O D S 
+		//
+
+		void		createVertexData()						override;
+		bool		createColorData(const Color& _c)		override;		/// Creates per-vertex color data, if the data were already created, the color will be changed (for all the view instantiated by the factory). The input color for this method is not referenced.
+		bool		createNormalData()						override;		/// Creates the normals at every vertex of the view
+		bool		create2DTextureMapData()				override;		/// Creates the data to be able to map a 2D texture on the whole view
+	
+	protected:
+		View*		createViewAndSetUpRenderParameters()	override;
+	};
+}
