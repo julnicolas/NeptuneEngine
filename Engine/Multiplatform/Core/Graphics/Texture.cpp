19b7714f90eba23e1e53fc6ab92115c0e3c0c08f
Julien Nicolas <juli3nnicolas@gmail.com>
2017-08-31 21:57:35 +0100

    Started to refactor the Texture class to be able to instanciate any kind of texture.

--- Engine/Multiplatform/Core/Graphics/Texture.cpp
+++ Engine/Multiplatform/Core/Graphics/Texture.cpp
@@ -5,27 +5,123 @@
 
 using namespace Neptune;
 
-Texture::Texture(const char* name): 
+
+// Use the library FreeImage for loading pretty much all formats
+// Add a ktx loader to be able to use this format.
+
+
+////////////////////////////////////////////////////////////////
+//
+//						S T A T I C S
+//
+////////////////////////////////////////////////////////////////
+
+static u16 MapTextureType(Texture::Type _type)
+{
+	u16 type = 0;
+	switch (_type)
+	{
+		// S I N G L E   T E X T U R E S
+	case Texture::Type::TEXTURE_1D:
+		type = GL_TEXTURE_1D;
+		break;
+
+	case Texture::Type::TEXTURE_2D:
+		type = GL_TEXTURE_2D;
+		break;
+
+	case Texture::Type::TEXTURE_3D:
+		type = GL_TEXTURE_3D;
+		break;
+
+	case Texture::Type::CUBE_MAP:
+		type = GL_TEXTURE_CUBE_MAP;
+		break;
+
+	case Texture::Type::BUFFER:
+		type = GL_TEXTURE_BUFFER;
+		break;
+
+	case Texture::Type::MULTI_SAMPLE_2D:
+		type = GL_TEXTURE_2D_MULTISAMPLE;
+		break;
+
+		// A R R A Y S
+	case Texture::Type::TEXTURE_ARRAY_1D:
+		type = GL_TEXTURE_1D_ARRAY;
+		break;
+
+	case Texture::Type::TEXTURE_ARRAY_2D:
+		type = GL_TEXTURE_2D_ARRAY;
+		break;
+
+	case Texture::Type::CUBE_MAP_ARRAY:
+		type = GL_TEXTURE_CUBE_MAP_ARRAY;
+		break;
+
+	case Texture::Type::TEXTURE_ARRAY_2D_MULTISAMPLE:
+		type = GL_TEXTURE_2D_MULTISAMPLE_ARRAY;
+		break;
+	}
+
+	return type;
+}
+
+
+////////////////////////////////////////////////////////////////
+//
+//			C L A S S   I M P L E M E N T A T I O N
+//
+////////////////////////////////////////////////////////////////
+
+Texture::Texture():
 	m_textureID(0),
 	m_width(0), 
 	m_height(0),
-	m_componentsPerPixel(0)
+	m_depth(0),
+	m_componentsPerPixel(0),
+	m_path(nullptr),
+	m_data(nullptr),
+	m_size(0),
+	m_type(Type::TEXTURE_2D)
 {
-	if ( name != nullptr )
-	{
-		m_name = new char[ strlen(name) ];
-		strcpy(m_name, name);
-	}
-	else
-	{
-		m_name  = new char[1];
-		*m_name = '\0';
-	}
+
+}
+
+Texture::Texture(const char* _path, Type _type /*= TextureType::TEXTURE_2D*/): 
+	Texture()
+{
+	setPath(_path);
+	m_type = _type;
+}
+
+Texture::Texture(void* _data, u32 _size, Type _type /*= TextureType::TEXTURE_2D*/):
+	Texture()
+{
+	m_size = _size;
+	m_data = _data;
+	m_type = _type;
 }
 
 Texture::~Texture()
 {
-	delete[] m_name;
+	delete[] m_path;
+}
+
+void Texture::setPath(const char* _path)
+{
+	NEP_ASSERT(_path != nullptr); // Path is invalid
+
+	m_path = new char[ strlen(_path) ];
+	strcpy(m_path, _path);
+}
+
+void Texture::setData(void* _data, u32 _size)
+{
+	NEP_ASSERT( _data != nullptr && _size > 0 ); // Wrong data
+
+	m_data = _data;
+	m_size = _size;
 }
 
 void Texture::CreateTexture(u8* data, s32 reqComp)
@@ -77,7 +173,7 @@ void Texture::SetPlaceHolderTexture(u8*& data)
 bool Texture::init()
 {
 	// Reading image's content
-	u8* data = stbi_load( m_name, &m_width, &m_height, &m_componentsPerPixel, 3);
+	u8* data = stbi_load( m_path, (s32*) &m_width, (s32*) &m_height, (s32*) &m_componentsPerPixel, 3);
 	if (data != nullptr)
 	{
 		CreateTexture( data, 3);
--- Engine/Multiplatform/Core/Graphics/Texture.h
+++ Engine/Multiplatform/Core/Graphics/Texture.h
@@ -8,29 +8,68 @@ namespace Neptune
 	class Texture final: public Updatable
 	{
 	public:
-		Texture(const char* name);
+		enum class Type : u8
+		{
+			TEXTURE_1D,
+			TEXTURE_2D,
+			TEXTURE_3D,
+			CUBE_MAP,
+			BUFFER,				/// Texture of really large size
+			MULTI_SAMPLE_2D,	/// Used for anti-aliasing filters
+			TEXTURE_ARRAY_1D,
+			TEXTURE_ARRAY_2D,
+			TEXTURE_ARRAY_2D_MULTISAMPLE,
+			CUBE_MAP_ARRAY,
+		};
+
+		/// 
+		/// \brief Default constructor. Defined for the class to be usable by STL containers.
+		/// \note Default texture type is TEXTURE_2D.
+		///
+		Texture();
+
+		/// Loads a texture from a file
+		Texture(const char* _path, Type _type = Type::TEXTURE_2D);
+		
+		/// 
+		/// \brief Loads a Texture from a buffer. The texture discard the buffer after init() is called.
+		/// \brief The content is then stored in VRAM.
+		///
+		Texture(void* _data, u32 _size, Type _type = Type::TEXTURE_2D);
+
 		~Texture();
-		Texture(const Texture& t)             = delete;
-		Texture& operator=(const Texture& t)  = delete;
+		Texture(const Texture& t)				= delete;
+		Texture(Texture&&)						= delete;
+		Texture& operator=(const Texture& t)	= delete;
+		Texture& operator=(Texture&&)			= delete;
 
 		bool init()      final override;
 		bool update()    final override;
 		void terminate() final override;
 
-		s32         getWidth()          const { return m_width;              }
-		s32         getHeight()         const { return m_height;             }
-		s32         getNbCompPerPixel() const { return m_componentsPerPixel; }
-		const char* getName()           const { return m_name;               }
-		bool        isInitialized()     const { return m_textureID != 0;     }
+		void setPath(const char* _path);
+		void setData(void* _data, u32 _size);
+		void setType(Type _type)				{	m_type = _type;					}
+
+		u32         getWidth()          const	{	return m_width;					}
+		u32         getHeight()         const	{	return m_height;				}
+		u32			getDepth()			const	{	return m_depth;					}
+		u32         getNbCompPerPixel() const	{	return m_componentsPerPixel;	}
+		const char* getName()           const	{	return m_path;					}
+		bool        isInitialized()     const	{	return m_textureID != 0;		}
 
 	private:
 		void CreateTexture(u8* data, s32 reqComp);
 		void SetPlaceHolderTexture(u8*& data);
 
-		u32   m_textureID;
-		s32   m_width;
-		s32   m_height;
-		s32   m_componentsPerPixel;
-		char* m_name;
+		u32				m_textureID;
+		u32				m_width;
+		u32				m_height;
+		u32				m_depth;																	/// Used for 3D textures
+		u32				m_componentsPerPixel;
+		Type			m_type;
+		u32				m_size;																		/// Size of the texture data
+		void*			m_data;																		/// Used when setting texture data at runtime rather than from a file
+		char*			m_path;
 	};
 }
