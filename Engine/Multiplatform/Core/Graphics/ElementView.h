bbfb7e9b1c5d374d46b7ef0e79395fcc6d27bd81
Julien Nicolas <juli3nnicolas@gmail.com>
2017-09-19 00:32:24 +0100

    Added method to index renderer's index buffer

--- Engine/Multiplatform/Core/Graphics/ElementView.h
+++ Engine/Multiplatform/Core/Graphics/ElementView.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Graphics/View.h"
+#include "Graphics/ElementRenderer.h"
 
 namespace Neptune
 {
@@ -11,5 +12,10 @@ namespace Neptune
 		virtual ~ElementView();
 		ElementView(const ElementView&)            = delete;
 		ElementView& operator=(const ElementView&) = delete;
+
+		void setIndexBufferData(const void* _data, u32 _dataSize, ElementRenderer::IndexType _dataType) {
+			 // The renderer MUST be an ElementRenderer, otherwise the class implementation is wrong.
+			static_cast<ElementRenderer*>( m_renderer )->setIndexBufferData(_data, _dataSize, _dataType);
+		}
 	};
 }
