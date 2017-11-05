38d9aacabb145c2f8c9ea82bfb954b1b83c590e8
Julien Nicolas <juli3nnicolas@gmail.com>
2017-11-05 21:09:36 +0000

    Updated a comment

--- Engine/Multiplatform/Core/Graphics/UniformVarNames.h
+++ Engine/Multiplatform/Core/Graphics/UniformVarNames.h
@@ -8,10 +8,23 @@
 
 
 // Space-transformation matrices
-#define NEP_UNIVNAME_MV_MATRIX     "ModelView"
-#define NEP_UNIVNAME_PROJ_MATRIX   "Projection"
+#define NEP_UNIVNAME_MV_MATRIX						"ModelView"
+#define NEP_UNIVNAME_PROJ_MATRIX					"Projection"
 
 // Lights
-#define NEP_UNIVNAME_AMBIENT_LIGHT				"AmbientLight"
-#define NEP_UNIVNAME_DIFFUSE_LIGHT_DIRECTION	"DiffuseLightDirection"
-#define NEP_UNIVNAME_DIFFUSE_LIGHT_COLOR		"DiffuseLightColor"
+#define NEP_UNIVNAME_AMBIENT_LIGHT					"AmbientLight"
+#define NEP_UNIVNAME_DIFFUSE_LIGHT_DIRECTION		"DiffuseLightDirection"
+#define NEP_UNIVNAME_DIFFUSE_LIGHT_COLOR			"DiffuseLightColor"
+
+// Multi-texturing
+
+/// \brief	This array is a set of pairs of the following
+///			<last_vertex_index, texture_binding_point>.
+///			Let's say one want to texture two plans with
+///			2 different textures of binding points 0 and 1.
+///			The array will be {5, 0, 11, 1} with the pair
+///			<5, 0> meaning - from vertex 0 to 5 use texture
+///			at binding point 0. Then pair <11, 1> meaning -
+///			from vertices 6 to 11 use texture at binding
+///			point 1.
+#define NEP_UNIVNAME_TEXTURE_BINDING_INDEX_ARRAY	"TextureBindingIndexArray"
