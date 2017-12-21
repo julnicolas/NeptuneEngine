0421cbc77cc619c80fdf5d65dc1b5fec4b2897c9
Julien Nicolas <juli3nnicolas@gmail.com>
2017-12-21 17:13:25 +0100

    Used template definitions instead of explicit overriding for min max functions

--- Engine/Multiplatform/Core/Math/Math.h
+++ Engine/Multiplatform/Core/Math/Math.h
@@ -1,24 +1,10 @@
 #pragma once
 
-#include "System/Type/Integers.h"
-
 namespace Neptune
 {
-	inline u64    Min(u64 x, u64 y)          { return (x < y) ? x : y; }
-	inline s64    Min(s64 x, s64 y)          { return (x < y) ? x : y; }
-	inline u32    Min(u32 x, u32 y)          { return (x < y) ? x : y; }
-	inline s32    Min(s32 x, s32 y)          { return (x < y) ? x : y; }
-	inline u8     Min(u8 x, u8 y)            { return (x < y) ? x : y; }
-	inline s8     Min(s8 x, s8 y)            { return (x < y) ? x : y; }
-	inline float  Min(float x, float y)      { return (x < y) ? x : y; }
-	inline double Min(double x, double y)    { return (x < y) ? x : y; }
+	template <typename T>
+	inline T Min(const T& t1, const T& t2)	{ return (t1 < t2) ? t1 : t2; }
 
-	inline u64    Max(u64 x, u64 y)          { return (x > y) ? x : y; }
-	inline s64    Max(s64 x, s64 y)          { return (x > y) ? x : y; }
-	inline u32    Max(u32 x, u32 y)          { return (x > y) ? x : y; }
-	inline s32    Max(s32 x, s32 y)          { return (x > y) ? x : y; }
-	inline u8     Max(u8 x, u8 y)            { return (x > y) ? x : y; }
-	inline s8     Max(s8 x, s8 y)            { return (x > y) ? x : y; }
-	inline float  Max(float x, float y)      { return (x > y) ? x : y; }
-	inline double Max(double x, double y)    { return (x > y) ? x : y; }
+	template <typename T>
+	inline T Max(const T& t1, const T& t2)	{ return (t1 > t2) ? t1 : t2; }
 }
