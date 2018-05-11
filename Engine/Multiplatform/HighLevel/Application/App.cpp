d4478d589c87ed866c3abb950e4a9c12d15de51e
Julien Nicolas <ju.nicodev@gmail.com>
2018-05-12 00:37:38 +0200

    Refactored the class App to integrate the reversed-z depth buffering technique

--- Engine/Multiplatform/HighLevel/Application/App.cpp
+++ Engine/Multiplatform/HighLevel/Application/App.cpp
@@ -4,6 +4,9 @@
 #include "System/Type/Integers.h"
 #include "Input/EventSystemInterface.h"
 
+// debug - must be moved to a platform-specific location
+#include <SDL2\SDL.h>
+
 using namespace Neptune;
 
 App::App(const char* _appName):
@@ -37,6 +40,74 @@ App::App():
 	m_appName = default_app_name;
 }
 
+bool App::enableReversedZ()
+{
+	//if ((major > 4 || (major == 4 && minor >= 5)) ||
+	if (!SDL_GL_ExtensionSupported("GL_ARB_clip_control"))
+	{
+		NEP_LOG("Error, Reversed-z is not supported.");
+		return false;
+	}
+	
+	// Override OpenGL default clipping policy -
+	// clipping is done between [-1.0, 1.0], here it is
+	// forced to [0.0, 1.0]
+	glClipControl(GL_LOWER_LEFT, GL_ZERO_TO_ONE);
+	
+	// Generate the colour buffer associated to the frame buffer
+	u32 color, depth, fbo;
+
+	glGenTextures(1, &color);
+	glBindTexture(GL_TEXTURE_2D, color);
+	glTexStorage2D(GL_TEXTURE_2D, 1, GL_SRGB8_ALPHA8, m_windowWidth, m_windowHeight);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	// Generate the depth buffer (with reversed-z enabled) that associates to the frame buffer
+	glGenTextures(1, &depth);
+	glBindTexture(GL_TEXTURE_2D, depth);
+	glTexStorage2D(GL_TEXTURE_2D, 1, GL_DEPTH_COMPONENT32F, m_windowWidth, m_windowHeight);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	// Create the frame buffer
+	glGenFramebuffers(1, &fbo);
+	glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, color, 0);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depth, 0);
+	GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+
+	if (status != GL_FRAMEBUFFER_COMPLETE) {
+		fprintf(stderr, "glCheckFramebufferStatus: %x\n", status);
+	}
+
+	// Bind the frame buffer
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	// Set depth buffer's clear-value
+	glClearDepth(0.0f); // 0.0f instead of 1.0f because reversed-z is used
+
+	// Fragments close to 1.0 are the ones close to the camera with the reversed-z technique
+	glDepthFunc(GL_GREATER);
+	
+	return true;
+}
+
+// Projection matrices
+void App::useStandardPerspectiveMatrix()
+{
+
+
+}
+
+void App::useReversedZPerspectiveMatrix()
+{
+
+}
+
+void App::useReversedZInfinityPerspectiveMatrix()
+{
+
+}
+
 bool App::init()
 {
 	m_window  = DisplayDeviceInterface::CreateWindow(m_appName, m_windowWidth, m_windowHeight);
@@ -55,23 +126,25 @@ bool App::update()
 	bool is_running = true;
 	while (is_running)
 	{
+		onFrameStart();
+
 		// Clear screen
 		DisplayDeviceInterface::ClearBuffers(background);
 
 		// Update controls
 		m_controller.update();
 
-		// TODO : Add user-function here (template method)
-		// should return true value for the main loop to continue, false otherwise.
-
 		// Draw views
 		for (auto& v : m_views)
 		{
+			forEachViewDo(v);
 			v->update();
 		}
 
 		// Display draw result
 		DisplayDeviceInterface::SwapBuffer(m_window);
+
+		onFrameEnd();
 	}
 
 	return true;
@@ -97,4 +170,19 @@ void App::terminate()
 App::~App()
 {
 	delete[] m_appName;
+}
+
+bool App::onFrameStart()
+{
+	return true;
+}
+
+bool App::onFrameEnd()
+{
+	return true;
+}
+
+bool App::forEachViewDo(View* _view)
+{
+	return true;
 }
--- Engine/Multiplatform/HighLevel/Application/App.h
+++ Engine/Multiplatform/HighLevel/Application/App.h
@@ -1,14 +1,5 @@
 #pragma once
 
-
-//////////////////////////////////////////
-//
-//    This file is being refactored
-//
-/////////////////////////////////////////
-
-
-//#include "Graphics/Renderer.h"
 #include "StdInterface/Updatable.h"
 #include "Graphics/DisplayDeviceInterface.h"
 #include "Graphics/Camera.h"
@@ -25,17 +16,48 @@ namespace Neptune
 	public:
 		App();
 		App(const char* _appName);
-		virtual ~App()             = default;
+		virtual ~App();
 		App(const App&)            = delete;
 		App& operator=(const App&) = delete;
 
-		void setWindowSize(u32 _height, u32 _width)	{ m_windowHeight = _height; m_windowWidth = _width; }
 
-		bool init()      override;			/// Prepares the app to be updated.
-		bool update()    override;			/// Executed once per frame.
-		void terminate() override;			/// Must be executed when the app exits from the main loop.
+		////////////////////////////////////////////////////////////////
+		//
+		//			P R E  -  I N I T   C A L L S
+		//
+		////////////////////////////////////////////////////////////////
+
+		void setWindowSize(u32 _height, u32 _width)	{ m_windowHeight = _height; m_windowWidth = _width; }
+		
+		/// Enable reversed-z technique.
+		/// \brief This feature enables large-view-rendering with few z-fighting issues.
+		/// \warning This technique is only available if GL_ARB_CLIP_CONTROL or OpenGL 4.5 is supported.
+		/// \note Nvidia and ATI cards support this feature since 2014 on Windows, 2015 on Linux.
+		/// For Intel, support for this feature starts in 2016.
+		bool enableReversedZ();
+
+		// Projection matrices
+		void useStandardPerspectiveMatrix();			/// Standard perspective projective matrix which projects scene's content between the near and far planes.
+		void useReversedZPerspectiveMatrix();			/// Set perspective projection matrix to be compatible with the reversed-z technique. Scene is projected between the near and far planes.
+		void useReversedZInfinityPerspectiveMatrix();	/// Set perspective projection matrix to be compatible with the reversed-z technique. Scene points to infinity.
+
+
+		////////////////////////////////////////////////////////////////
+		//
+		//			A P P   S T A T E   M A N A G E M E N T
+		//
+		////////////////////////////////////////////////////////////////
+
+		bool init()      override;						/// Prepares the app to be updated.
+		bool update()    override;						/// Executed once per frame.
+		void terminate() override;						/// Must be executed when the app exits from the main loop.
 
 	protected:
+		// User frame control
+		virtual bool onFrameStart();					/// Called before anything else in the main loop
+		virtual bool onFrameEnd();						/// Last function called in the main loop
+		virtual bool forEachViewDo(View* _view);		/// Called for each view, right before View::update() is called.
+
 		const char*										m_appName;
 		u32												m_windowHeight;
 		u32												m_windowWidth;
