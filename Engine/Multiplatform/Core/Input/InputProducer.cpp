5b2affcf8450d6fd9057ec52c543f5e34ecd8adf
Julien Nicolas <ju.nicodev@gmail.com>
2018-11-11 15:44:50 +0100

    add/remove input queues on subscriber addition/deletion

--- Engine/Multiplatform/Core/Input/InputProducer.cpp
+++ Engine/Multiplatform/Core/Input/InputProducer.cpp
@@ -8,16 +8,26 @@ using namespace Neptune;
 
 void InputProducer::addConsumer(InputConsumer* _consumer, InputType _type)
 {
+	// Add the consumer to the consumer list. Consumers are sorted by event types.
 	m_consumer_list[_type].push_back(_consumer);
+
+	// Instantiate an input queue if it doesn't already exist
+	m_input_list.emplace(_type);
 }
 
 void InputProducer::rmConsumer(InputConsumer* _consumer, InputType _type)
 {
+	// Delete the consumer so that it doesn't subscribe to the input feed any more
 	std::vector<InputConsumer*>& consumers = m_consumer_list[_type];
 	std::vector<InputConsumer*>::iterator it = std::find(consumers.begin(), consumers.end(), _consumer);
 	NEP_ASSERT_ERR_MSG(it != consumers.end(), "Couldn't find consumer in m_consumer_list");
 
 	consumers.erase(it);
+
+	// If the consumer list is empty, it means they are not any subscribers for
+	// the input of type _type. So the associated input queue must be deleted.
+	if (consumers.empty())
+		m_input_list.erase(_type);
 }
 
 void InputProducer::update()
